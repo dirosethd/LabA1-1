392aedf303d29b92348f411774c5bfdad66a46e5
dirose <dirose@DESKTOP-PQA70RD>
2024-10-10 14:13:12 +0200

    Добавьте файлы проекта.

---
+++ laba1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+
+    Console.Write("Введите значение y: ");
+    double y = double.Parse(Console.ReadLine());
+
+
+    Console.Write("Введите значение p: ");
+    double p = double.Parse(Console.ReadLine());
+
+
+    Console.Write("Введите значение n: ");
+    double n = double.Parse(Console.ReadLine());
+
+
+    double N = 3 * Math.Pow(y, 2) + Math.Sqrt(y + 1) / Math.Log(p + n) + Math.Exp(p);
+
+    Console.WriteLine($"Результат N = {N}");
+
+}
+catch
+{
+    Console.WriteLine("Введите правильно");
+}
